f354751c27dde8e73ac2b12cb8ffbf46f991e9da
Calogero Costa <calogero.costa@garr.it>
2026-09-16 11:54:00 +0200

    Added IDEM Assurance Profile checks

--- docker/web/shared/index.php
+++ docker/web/shared/index.php
@@ -1,7 +1,7 @@
 <?php
 $page_title = "SAML Login Details";
-include('/var/www/html/sp/shared/header.php');
-include('/var/www/html/sp/shared/logout.php');
+include_once '/var/www/html/sp/shared/header.php';
+include_once '/var/www/html/sp/shared/logout.php';
 ?>
 
 <h2 class="garr-text-warning mb-4 text-center">SAML Login Details</h2>
@@ -88,7 +88,7 @@ include('/var/www/html/sp/shared/logout.php');
                     "schacUserStatus" => "urn:oid:1.3.6.1.4.1.25178.1.2.19",
                     "schacProjectMembership" => "urn:oid:1.3.6.1.4.1.25178.1.2.20",
                     "schacProjectSpecificRole" => "urn:oid:1.3.6.1.4.1.25178.1.2.21",
-                    "schacYearOfBirth" => "urn:oid:1.3.6.1.4.1.25178.1.0.2.3",                    
+                    "schacYearOfBirth" => "urn:oid:1.3.6.1.4.1.25178.1.0.2.3",
                     
                     // Other Common Person Attributes
                     "audio" => "urn:oid:0.9.2342.19200300.100.1.55",
@@ -150,6 +150,207 @@ include('/var/www/html/sp/shared/logout.php');
     </table>
 </div>
 
+<br>
+<div class="w-75 mx-auto">
+    <h4 class="garr-text-warning mb-4">Assurance</h4>
+    <table class="table table-bordered table-striped" style="table-layout: fixed; width: 100%">
+        <thead>
+            <tr>
+                <th class="garr-bar text-white" style="width: 32%">Name</th>
+                <th class="garr-bar text-white" style="width: 17%">Declared</th>
+		<th class="garr-bar text-white" style="width: 17%">Authentication</th>
+		<th class="garr-bar text-white" style="width: 17%">Defined Values</th>
+		<th class="garr-bar text-white" style="width: 17%">Profile Validity</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach ($_SERVER as $key => $value) {
+                    if(strpos($key, 'urn:') === 0 || array_key_exists($key, $attributeUrnMap)) {
+                        if($key == "eduPersonAssurance"){
+                            $metaValues = explode(';', $value);
+
+                        $affiliation_attr = false;
+
+                        foreach ($_SERVER as $second_key => $second_value) {
+                            if(strpos($second_key, 'urn:') === 0 || array_key_exists($second_key, $attributeUrnMap)) {
+                                if($second_key == "eduPersonAffiliation" || $second_key == "eduPersonScopedAffiliation"){
+                                    $affiliation_attr = true;
+                                }
+                            }
+                        }
+
+			            // Loop through all $_SERVER variables to find AuthnContext-Class
+                        foreach ($_SERVER as $server_key => $server_value) {
+                            if (strpos($server_key, 'Shib-') === 0) {
+                                $clean_key2 = substr($server_key, 5);
+
+                                if($clean_key2 == "AuthnContext-Class"){
+                                    $authnContext_class = $server_value;
+
+					                // Declaring all possible levels of assurance
+                                    $assurance_lvl = ["https://idem.garr.it/af/IDEM-P0","https://idem.garr.it/af/IDEM-P1","https://idem.garr.it/af/IDEM-P2","https://idem.garr.it/af/IDEM-P3"];
+
+                                    $idemp0 = false;
+                                    $idemp1 = false;
+                                    $idemp2 = false;
+                                    $idemp3 = false;
+
+                                    foreach($assurance_lvl as $al){
+
+                                        // Dichiarato
+                                        if (in_array($al, $metaValues, true)){
+                                            echo "<tr><td class=\"text-break\"><strong>{$al}</strong><td class=\"cell text-break\">True</td>";
+                                        } else {
+                                        // Non Dichiarato
+                                            echo "<tr><td class=\"text-break\"><strong>{$al}</strong><td class=\"cell text-break\">False</td>";
+                                        }
+
+                                        // Tipologia Autenticazione
+                                        if ($authnContext_class == "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport" || $authnContext_class == "https://refeds.org/profile/sfa"){
+                                            echo "<td class=\"cell text-break\">Single Factor</td>";
+                                        } else {
+                                            if ($authnContext_class == "https://refeds.org/profile/mfa"){
+                                                echo "<td class=\"cell text-break\">Multi Factor</td>";
+                                            }
+                                        }
+
+				            	        // Checking IDEM-P0 Required Attributes
+                                        if($al == "https://idem.garr.it/af/IDEM-P0"){
+                                            if (in_array("https://refeds.org/assurance", $metaValues, true) &&
+                                                in_array("https://refeds.org/assurance/ID/unique", $metaValues, true) &&
+                                                in_array("https://refeds.org/assurance/ID/eppn-unique-no-reassign", $metaValues, true) &&
+                                                in_array("https://refeds.org/assurance/IAP/low", $metaValues, true) &&
+                                                in_array("https://idem.garr.it/af/IDEM-P0", $metaValues, true)) {
+                                                $idemp0 = true;
+                                            }
+
+                                            //Attributes Declared
+                                            if ($idemp0){
+                                                echo "<td class=\"cell text-break\">True</td>";
+                                            } else {
+                                            //Attributes Not Declared
+                                                echo "<td class=\"cell text-break\">False</td>";
+                                            }
+
+                                            //Valido
+                                            if ($idemp0 && ($authnContext_class == "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport" || $authnContext_class == "https://refeds.org/profile/sfa" || $authnContext_class == "https://refeds.org/profile/mfa")){
+                                                echo "<td class=\"cell text-break\">True</td></tr>";
+                                            } else {
+                                            //Non Valido
+                                                echo "<td class=\"cell text-break\">False</td></tr>";
+                                            }
+                                        }
+
+					                    // Checking IDEM-P1 Required Attributes
+                                        if ($al == "https://idem.garr.it/af/IDEM-P1"){
+
+                                            if($idemp0){
+                                                if ($affiliation_attr) {
+                                                    if (in_array("https://refeds.org/assurance/IAP/medium", $metaValues, true) &&
+                                                        in_array("https://refeds.org/assurance/profile/cappuccino", $metaValues, true) &&
+                                                        in_array("https://refeds.org/assurance/ATP/ePA-1m", $metaValues, true) &&
+                                                        in_array("https://idem.garr.it/af/IDEM-P1", $metaValues, true)){
+                                                            $idemp1 = true;
+                                                        }
+                                                } else {
+                                                    if (in_array("https://refeds.org/assurance/IAP/medium", $metaValues, true) &&
+                                                        in_array("https://refeds.org/assurance/profile/cappuccino", $metaValues, true) &&
+                                                        in_array("https://idem.garr.it/af/IDEM-P1", $metaValues, true)){
+                                                        $idemp1 = true;
+                                                    }
+                                                }
+                                            }
+
+						                    //Attributi Dichiarati
+                                            if ($idemp1){
+                                                echo "<td class=\"cell text-break\">True</td>";
+                                            } else {
+                                            //Attributi Non Dichiarati
+                                                echo "<td class=\"cell text-break\">False</td>";
+                                            }
+
+                                            //Valido
+                                            if ($idemp1 && ($authnContext_class == "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport" || $authnContext_class == "https://refeds.org/profile/sfa" || $authnContext_class == "https://refeds.org/profile/mfa")){
+                                                echo "<td class=\"cell text-break\">True</td></tr>";
+                                            } else {
+                                            //Non Valido
+                                                echo "<td class=\"cell text-break\">False</td></tr>";
+                                            }
+                                        }
+
+					                    // Checking IDEM-P2 Required Attributes
+                                        if ($al == "https://idem.garr.it/af/IDEM-P2"){
+
+                                            if($idemp1){
+                                                if ($affiliation_attr) {
+                                                    if (in_array("https://refeds.org/assurance/IAP/high", $metaValues, true) &&
+                                                        in_array("https://refeds.org/assurance/profile/espresso", $metaValues, true) &&
+                                                        in_array("https://refeds.org/assurance/ATP/ePA-1d", $metaValues, true) &&
+                                                        in_array("https://idem.garr.it/af/IDEM-P2", $metaValues, true)){
+                                                        $idemp2 = true;
+                                                    }
+                                                } else {
+                                                    if (in_array("https://refeds.org/assurance/IAP/high", $metaValues, true) &&
+                                                        in_array("https://refeds.org/assurance/profile/espresso", $metaValues, true) &&
+                                                        in_array("https://idem.garr.it/af/IDEM-P2", $metaValues, true)){
+                                                        $idemp2 = true;
+                                                    }
+                                                }
+                                            }
+
+                    						//Attributi Dichiarati
+                                            if ($idemp2){
+                                                echo "<td class=\"cell text-break\">True</td>";
+                                            } else {
+                                            //Attributi Non Dichiarati
+                                                echo "<td class=\"cell text-break\">False</td>";
+                                            }
+
+                                            //Valido
+                                            if ($idemp2 && $authnContext_class == "https://refeds.org/profile/mfa"){
+                                                echo "<td class=\"cell text-break\">True</td></tr>";
+                                            } else {
+                                                //Non Valido
+                                                echo "<td class=\"cell text-break\">False</td></tr>";
+                                            }
+                                        }
+
+            				            // Checking IDEM-P3 Required Attributes
+                                        if ($al == "https://idem.garr.it/af/IDEM-P3"){
+                                            if($idemp2){
+                                                if(in_array("https://idem.garr.it/af/IDEM-P3", $metaValues, true)){
+                                                    $idemp3 = true;
+                                                }
+                                            }
+
+                			    			//Attributi Dichiarati
+                                            if ($idemp3){
+                                                echo "<td class=\"cell text-break\">True</td>";
+                                            } else {
+                                                //Attributi Non Dichiarati
+                                                echo "<td class=\"cell text-break\">False</td>";
+                                            }
+
+                                            //Valido
+                                            if ($idemp3 && $authnContext_class == "https://refeds.org/profile/mfa"){
+                                                echo "<td class=\"cell text-break\">True</td></tr>";
+                                            } else {
+                                            //Non Valido
+                                                echo "<td class=\"cell text-break\">False</td></tr>";
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } ?>
+        </tbody>
+    </table>
+</div>
+
 <div class="w-75 mx-auto">
     <br>
     <h4 class="garr-text-warning mb-4">Session</h4>
@@ -183,7 +384,7 @@ include('/var/www/html/sp/shared/logout.php');
     </div>
 </div>
 
-<?php include('/var/www/html/sp/shared/footer.html'); ?>
+<?php include_once '/var/www/html/sp/shared/footer.html'; ?>
 
 </body>
 </html>
